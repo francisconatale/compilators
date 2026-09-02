af6f959e05c550b6032dc08bcbce999111d72c40
Francisco Natale <francisco_natale@hotmail.com>
2026-09-02 17:35:18 -0300

    reorder structures

--- preProject/agus/ast.c
+++ preProject/agus/ast.c
@@ -3,104 +3,97 @@
 #include <string.h>
 #include "ast.h"
 
-// Este archivo contiene las funciones auxiliares que permiten crear los nodos definidos en ast.h
-// Estas funciones las vamos a usar en Bison para construir el arbol sintactico a medida que parseamos
-// el programa
-
-// Por ej: para el nodo del programa, le pasamos los nodos de los statements
-// Creamos el nodo raiz del programa, le asignamos su tipo, y le asignamos la lista de statements
 ASTNode* create_program_node(ASTNode* statements) {
     ASTNode* node = (ASTNode*) malloc(sizeof(ASTNode));
     node->type = PROGRAM_NODE;
-    node->data.program.statements = statements;
+    node->as.program.statements = statements;
     return node;
 }
 
 ASTNode* create_statement_list_node(ASTNode* statement, ASTNode* next) {
     ASTNode* node = (ASTNode*) malloc(sizeof(ASTNode));
     node->type = STATEMENT_LIST_NODE;
-    node->data.statement_list.statement = statement;
-    node->data.statement_list.next = next;
+    node->as.statement_list.statement = statement;
+    node->as.statement_list.next = next;
     return node;
 }
 
 ASTNode* create_declaration_node(int data_type, char* var_name) {
     ASTNode* node = (ASTNode*) malloc(sizeof(ASTNode));
     node->type = DECLARATION_NODE;
-    node->data.declaration.data_type = data_type;
-    node->data.declaration.var_name = var_name;
+    node->as.declaration.data_type = data_type;
+    node->as.declaration.var_name = var_name;
     return node;
 }
 
 ASTNode* create_assignment_node(char* var_name, ASTNode* expression) {
     ASTNode* node = (ASTNode*) malloc(sizeof(ASTNode));
     node->type = ASSIGN_NODE;
-    node->data.assignment.var_name = var_name;
-    node->data.assignment.expression = expression;
+    node->as.assignment.var_name = var_name;
+    node->as.assignment.expression = expression;
     return node;
 }
 
 ASTNode* create_return_node(ASTNode* expression) {
     ASTNode* node = (ASTNode*) malloc(sizeof(ASTNode));
     node->type = RETURN_NODE;
-    node->data.return_stmt.expression = expression;
+    node->as.return_stmt.expression = expression;
     return node;
 }
 
 ASTNode* create_binop_node(ASTNode* left, char op, ASTNode* right) {
     ASTNode* node = (ASTNode*) malloc(sizeof(ASTNode));
     node->type = BINOP_NODE;
-    node->data.binop.left = left;
-    node->data.binop.op = op;
-    node->data.binop.right = right;
+    node->as.binop.left = left;
+    node->as.binop.op = op;
+    node->as.binop.right = right;
     return node;
 }
 
 ASTNode* create_constant_node(int value) {
     ASTNode* node = (ASTNode*) malloc(sizeof(ASTNode));
     node->type = CONSTANT_NODE;
-    node->data.constant.value = value;
+    node->as.constant.value = value;
     return node;
 }
 
 ASTNode* create_id_node(char* var_name) {
     ASTNode* node = (ASTNode*) malloc(sizeof(ASTNode));
     node->type = ID_NODE;
-    node->data.id.var_name = var_name;
+    node->as.id.var_name = var_name;
     return node;
 }
 
-// Por último, definimos la funcion de liberar memoria
 void free_ast(ASTNode* node) {
     if (node == NULL) {
         return;
     }
     switch (node->type) {
         case PROGRAM_NODE:
-            free_ast(node->data.program.statements);
+            free_ast(node->as.program.statements);
             break;
         case STATEMENT_LIST_NODE:
-            free_ast(node->data.statement_list.statement);
-            free_ast(node->data.statement_list.next);
+            free_ast(node->as.statement_list.statement);
+            free_ast(node->as.statement_list.next);
             break;
         case DECLARATION_NODE:
-            free(node->data.declaration.var_name);
+            free(node->as.declaration.var_name);
             break;
         case ASSIGN_NODE:
-            free(node->data.assignment.var_name);
-            free_ast(node->data.assignment.expression);
+            free(node->as.assignment.var_name);
+            free_ast(node->as.assignment.expression);
             break;
         case RETURN_NODE:
-            free_ast(node->data.return_stmt.expression);
+            free_ast(node->as.return_stmt.expression);
             break;
         case BINOP_NODE:
-            free_ast(node->data.binop.left);
-            free_ast(node->data.binop.right);
+            free_ast(node->as.binop.left);
+            free_ast(node->as.binop.right);
             break;
         case CONSTANT_NODE:
             break;
         case ID_NODE:
-            free(node->data.id.var_name);
+            free(node->as.id.var_name);
             break;
     }
     free(node); 
--- preProject/agus/ast.h
+++ preProject/agus/ast.h
@@ -1,99 +1,70 @@
-/*
-
-Con Flex y Bison, definiamos cuales eran los tokens del programa parseado. Los tokens son los átomos
-del programa, por ejemplo un número, un identificador, un operador, una palabra reservada, etc.
-
-Es decir, una expresion como "int x = 3" tiene distintos tokens: "int", "x", "=", "3"
-
-Eso es util para parsear el programa y validar su sintaxis.
-
-En cambio, los AST (Abstract Syntax Tree) son una representación estructurada del programa, donde los nodos 
-representan construcciones sintácticas relevantes del lenguaje en lugar de tokens individuales (por ejemplo,
-una declaración completa, una sentencia completa, etc. Eso nos permite después poder definir
-el orden y prioridad de las operaciones.
-
-Entonces, definimos nodos para cada unidad "logica" del programa:
-    - Un nodo raiz que es el Program Node
-    - De ahí, varios nodos para cada sentencia o declaración
-    - De esas sentencias, un nodo para las declaraciones de variables (completas, por ej., "int x;"), 
-      un nodo para las asignaciones (completas, por ej., "x =3;"), etc.
-
-De manera similar a como hacemos arboles sintacticos para definir la prioridad de operadores en expresiones
-aritméticas o lógicas, construimos un árbol para el programa. Eso nos permite más adelante hacer un análisis
-semántico y determinar si el programa es correcto (por ej., si todas las variables están definidas antes de ser
-usadas, etc.), determinar el orden en el que se deben ejecutar las instrucciones, generar el código
-intermedio, o el código máquina, etc.
-
-
-En este archivo definimos los tipos de nodos, su estructura, y los perfiles de las funciones que usaremos en Bison para construir el AST
-
-*/
+#ifndef AST_H
+#define AST_H
 
 typedef enum Node_Type {
-    PROGRAM_NODE,        // Represents the root of the program (main function)
-    STATEMENT_LIST_NODE, // Represents a sequence of sentences (Code block)
-    DECLARATION_NODE,    // Represents a variable declaration (int x;)
-    ASSIGN_NODE,         // Represents an assignment (x = 5;)
-    RETURN_NODE,         // Represents a return statement (return x;)
-    BINOP_NODE,          // Represents a binary operation (+, -, *)
-    CONSTANT_NODE,       // Represents a literal value (number or boolean)
-    ID_NODE              // Represents a variable identifier used in an expression
-
-    // Hay algunos nodos que no hacen falta
-    // No son necesarios nodos de tipo: el tipo sera un atributo de un nodo general
-    // No son necesarios nodos distintos para "return ALGO" o simplemente "return": en el segundo caso el hijo del nodo sera null
-    // etc.
-
+    PROGRAM_NODE,
+    STATEMENT_LIST_NODE,
+    DECLARATION_NODE,
+    ASSIGN_NODE,
+    RETURN_NODE,
+    BINOP_NODE,
+    CONSTANT_NODE,
+    ID_NODE
 } Node_Type;
 
+typedef struct ASTNode ASTNode;
 
-// Esta es la estructura de un nodo cualquiera
-// Cada campo de este struct permite almacenar la información necesaria para cada tipo de nodo
-// El union significa que un ASTNode sera alguno de todos los structs incluidos en el union.
-typedef struct ASTNode {
+typedef struct {
+    ASTNode* statements;
+} ASTProgram;
+
+typedef struct {
+    ASTNode* statement;
+    ASTNode* next;
+} ASTStatementList;
+
+typedef struct {
+    int data_type;
+    char* var_name;
+} ASTDeclaration;
+
+typedef struct {
+    char* var_name;
+    ASTNode* expression;
+} ASTAssignment;
+
+typedef struct {
+    ASTNode* expression;
+} ASTReturn;
+
+typedef struct {
+    ASTNode* left;
+    char op;
+    ASTNode* right;
+} ASTBinOp;
+
+typedef struct {
+    int value;
+} ASTConstant;
+
+typedef struct {
+    char* var_name;
+} ASTId;
+
+struct ASTNode {
     Node_Type type;
 
     union {
-        struct {
-            struct ASTNode* statements;
-        } program;
-
-        struct {
-            struct ASTNode* statement;
-            struct ASTNode* next;
-        } statement_list; // secuencia de sentencias
-
-        struct {
-            int data_type; // por ej., 0 para int, 1 para bool
-            char* var_name;
-        } declaration;
-        
-        struct {
-            char* var_name;
-            struct ASTNode* expression;
-        } assignment;
-        
-        struct {
-            struct ASTNode* expression; // puede ser NULL si es solo "return;"
-        } return_stmt; // return statement
-
-        struct {
-            struct ASTNode* left;
-            char op; // Usamos 'char' en lugar de 'char*' porque es solo '+', '-', '*'
-            struct ASTNode* right;
-        } binop; // binary operation
-
-        struct {
-            int value; // contiene el numero o el valor booleano (0 o 1)
-        } constant; // numero o bool
-
-        struct {
-            char* var_name;
-        } id; // variable usada dentro de una expresion
-
-    } data; // le damos un nombre al union (como 'data') para acceder a sus campos
-
-} ASTNode; // cerramos el struct correctamente
+        ASTProgram       program;
+        ASTStatementList statement_list;
+        ASTDeclaration   declaration;
+        ASTAssignment    assignment;
+        ASTReturn        return_stmt;
+        ASTBinOp         binop;
+        ASTConstant      constant;
+        ASTId            id;
+    } as;
+};
 
 ASTNode* create_program_node(ASTNode* statements);
 ASTNode* create_statement_list_node(ASTNode* statement, ASTNode* next);
@@ -104,3 +75,5 @@ ASTNode* create_binop_node(ASTNode* left, char op, ASTNode* right);
 ASTNode* create_constant_node(int value);
 ASTNode* create_id_node(char* var_name);
 void free_ast(ASTNode* node);
+
+#endif // AST_H
